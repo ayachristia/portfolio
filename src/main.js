302607d02358822d72ad9d7bc5e7ff53d5b48827
iamayac <ayachristia@gmail.com>
2025-05-22 09:48:50 +0200

    splash activated again

--- src/main.js
+++ src/main.js
@@ -7,28 +7,20 @@ import about from './components/about/about.js';
 import projects from './components/projects/projects.js';
 import scroll from './components/projects/projectslider.js';
 
-// window.addEventListener('load', () => {
-//     setTimeout(() => {
-//         layout();
-//     }, 3000);
+
 console.log(projects);
 window.onload = function () {
-    // Force scroll to top on page refresh
-    if (performance.navigation.type === 1) { // 1 indicates refresh
+    if (performance.navigation.type === 1) {
         window.scrollTo(0, 0);
     }
 
-    // Also handle fragment removal
     if (window.location.hash) {
-        // Remove the fragment
         history.pushState("", document.title, window.location.pathname + window.location.search);
-        // Ensure scroll to top
         window.scrollTo(0, 0);
     }
 };
-// });
 layout()
-// splash()
+splash()
 
 
 
@@ -36,9 +28,7 @@ document.querySelector('header').append(navigation())
 document.querySelector('main').append(hero())
 document.querySelector('main').append(about())
 document.querySelector('main').append(projects())
-// document.querySelector('main').appendChild(scroll())
 
-// test()
 
 
 
